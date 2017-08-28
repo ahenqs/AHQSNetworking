6db924fe6956cca8cb04f1cf7598ee59dd53a5c7
DA SILVA André Henrique <andre.henrique@consultant.volvo.com>
2017-08-28 15:52:02 +0200

    Add Postable

---
+++ AHQSNetworking/Postable.swift
@@ -0,0 +1,14 @@
+//
+//  Postable.swift
+//  AHQSNetworking
+//
+//  Created by DA SILVA Andre Henrique on 28/08/2017.
+//  Copyright © 2017 a h e n q s. All rights reserved.
+//
+
+import Foundation
+
+public protocol Postable {
+    associatedtype T
+    func post(dictionary: [String: Any], completion: @escaping (Result<T>) -> Void)
+}
